18a239a087c3de6c8f438a9243320bfd93fc97f8
Gerald <gera2ld@163.com>
2018-07-01 22:04:34 +0800

    fix: add case for texture

---
+++ src/index/cases/c006.js
@@ -0,0 +1,66 @@
+import { initCanvas, initShaderProgram } from '../util';
+
+const vsSource = `
+attribute vec4 a_Position;
+attribute vec2 a_TexCoord;
+varying vec2 v_TexCoord;
+void main() {
+  gl_Position = a_Position;
+  v_TexCoord = a_TexCoord;
+}
+`;
+const fsSource = `
+precision mediump float;
+uniform sampler2D u_Sampler;
+varying vec2 v_TexCoord;
+void main() {
+  gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+}
+`;
+
+export default async function init(container) {
+  const { gl } = initCanvas(container, 600, 600);
+  const program = initShaderProgram(gl, vsSource, fsSource);
+
+  const bufferData = new Float32Array([
+    -0.5, 0.5, -0.3, 1.7,
+    -0.5, -0.5, -0.3, -0.2,
+    0.5, 0.5, 1.7, 1.7,
+    0.5, -0.5, 1.7, -0.2,
+  ]);
+  const fSize = bufferData.BYTES_PER_ELEMENT;
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+
+  const aPosition = gl.getAttribLocation(program, 'a_Position');
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, fSize * 4, 0);
+  gl.enableVertexAttribArray(aPosition);
+  const aTexCoord = gl.getAttribLocation(program, 'a_TexCoord');
+  gl.vertexAttribPointer(aTexCoord, 2, gl.FLOAT, false, fSize * 4, fSize * 2);
+  gl.enableVertexAttribArray(aTexCoord);
+
+  const image = await loadImage('/public/kenny.png');
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+  gl.activeTexture(gl.TEXTURE0);
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  const uSampler = gl.getUniformLocation(program, 'u_Sampler');
+  gl.uniform1i(uSampler, 0);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function loadImage(src) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.onload = () => resolve(image);
+    image.src = src;
+  });
+}

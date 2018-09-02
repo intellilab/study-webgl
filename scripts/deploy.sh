4ff02e531cea106df3ad5c6a6d4ffa39b8a865f8
Gerald <gera2ld@163.com>
2018-09-02 12:26:03 +0800

    chore: update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ git init
 git config user.name Gerald
 git config user.email gera2ld@163.com
 git add .
-git commit -m 'Auto deploy to gitee-pages'
-git push -f git@gitee.com:gerald/study-webgl.git master:pages
+git commit -m 'Auto deploy to gh-pages'
+git push -f git@github.com:intellilab/study-webgl.git master:gh-pages
